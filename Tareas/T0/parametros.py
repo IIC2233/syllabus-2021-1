a2aa9106c1556087fd4cd9a7c81d4d8ad1f3bb63
FRANCISCA IBARRA <faibarra1@uc.cl>
2021-03-25 19:52:41 -0300

    Tarea 1 publicada :email: :left_speech_bubble:

---
+++ Tareas/T0/parametros.py
@@ -0,0 +1,7 @@
+# Si el programa detecta que se ingresó este input en una conversación de grupo
+# deberá sacar al usuario del grupo.
+ABANDONAR_FRASE = "\\salir"
+
+# Si el programa detecta que se ingresó este input en cualquier conversación,
+# debe volver al menú anterior.
+VOLVER_FRASE = "\\volver"
