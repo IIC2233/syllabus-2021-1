ba1e251ef30080df0e1b424d48dae43d57eae52f
Fran <faibarra1@uc.cl>
2021-06-29 13:01:41 -0400

    Tarea 3 publicada :battery::airplane:

---
+++ Tareas/T3/codificacion.py
@@ -0,0 +1,29 @@
+"""
+Modulo para funciones de codificacion y decodificacion para envio de mensajes.
+Recuerda, no debes modificar los argumentos que recibe cada funcion,
+y debes entregar exactamente lo que esta pide en el enunciado.
+"""
+
+
+# Codificar un mensaje a un bytearray segun el protocolo especificado.
+def codificar_mensaje(mensaje):
+    # COMPLETAR ESTA FUNCION
+    pass
+
+
+# Decodificar un bytearray para obtener el mensaje original.
+def decodificar_mensaje(mensaje):
+    # COMPLETAR ESTA FUNCION
+    pass
+
+
+# Codificar una imagen a un bytearray segun el protocolo especificado.
+def codificar_imagen(ruta):
+    # COMPLETAR ESTA FUNCION
+    pass
+
+
+# Decodificar un bytearray a una lista segun el protocolo especificado.
+def decodificar_imagen(mensaje):
+    # COMPLETAR ESTA FUNCION
+    pass
